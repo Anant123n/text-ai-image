39912fb202fd4b2d0fc5a2ce430d2edadd189dc1
Anant123n <anantagrawal140@gmail.com>
2025-09-10 12:40:49 +0530

    component made

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <div className='bg-amber-600'>axa</div>
+    </>
+  )
+}
+
+export default App
